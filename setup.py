1ceb518b1dc2f5cc526417f9c65fa30c30fd3f88
Antoine Bertin <diaoulael@gmail.com>
2013-07-16 02:15:29 +0200

    Remove test dependency to pyserial

--- setup.py
+++ setup.py
@@ -21,5 +21,4 @@ setup(name='hachi',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries :: Python Modules'],
-    test_suite='hachi.tests.suite',
-    tests_require=['pyserial'])
+    test_suite='hachi.tests.suite')
